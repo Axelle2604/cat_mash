4beef2c43cd76c3de4681cfdf8acd3c01b3d9006
Clemo2604 <axelle.dru@stackerine.com>
2019-07-08 15:13:14 +0200

    serve statics

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use('/api/v1', route);
 
+app.use('/', express.static(`${__dirname}/../client/build/`));
+
 const server = app.listen(process.env.PORT || 8080, () => {
   console.log(`Server started on port ${server.address().port}`);
 });
