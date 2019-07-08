7dafcbb0ede4f23f680a6856e42aa3a1d8498ca5
Clemo2604 <axelle.dru@stackerine.com>
2019-07-08 14:58:18 +0200

    Settings server port

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.use('/api/v1', route);
 
-const server = app.listen(8080, () => {
+const server = app.listen(process.env.PORT || 8080, () => {
   console.log(`Server started on port ${server.address().port}`);
 });
