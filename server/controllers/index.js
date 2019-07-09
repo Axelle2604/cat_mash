3152c4d6f567984c059d19d9012df6ad0e974f79
Clemo2604 <axelle.dru@stackerine.com>
2019-07-09 12:25:18 +0200

    Refacto server controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,37 +2,53 @@ const _ = require('lodash');
 const { getCatsFromJSON, updateScores, getScores } = require('../store/index');
 
 const getCats = async (req, res) => {
-  const cats = await getCatsFromJSON();
-  res.send(cats).status(200);
+  try {
+    const cats = await getCatsFromJSON();
+    res.send(cats).status(200);
+  } catch {
+    res.status(500);
+  }
 };
 
 const getCatsWithLimit = async (req, res) => {
-  const limitNumber = req.query.limit;
-  const cats = await getCatsFromJSON();
-  const shuffledCats = _.shuffle(cats);
-  const someCats = shuffledCats.slice(0, limitNumber);
-  res.send(someCats).status(200);
+  try {
+    const limitNumber = req.query.limit;
+    const cats = await getCatsFromJSON();
+    const shuffledCats = _.shuffle(cats);
+    const someCats = shuffledCats.slice(0, limitNumber);
+    res.send(someCats).status(200);
+  } catch {
+    res.status(500);
+  }
 };
 
 const updateCatsScores = async (req, res) => {
-  const catsScores = req.body;
-  updateScores(catsScores);
-  res.sendStatus(200);
+  try {
+    const catsScores = req.body;
+    updateScores(catsScores);
+    res.sendStatus(201);
+  } catch {
+    res.status(500);
+  }
 };
 
 const getCatsScores = async (req, res) => {
-  const { offset, limit } = req.query;
-  const scores = await getScores(offset, limit);
-  const catsScores = scores.map(
-    ({ points, nb_matchs: nbMatchs, cat_id: catId }) => {
-      return {
-        winRate: points > 0 ? winRateCalcul(points, nbMatchs) : 0,
-        points,
-        catId,
-      };
-    }
-  );
-  res.send(catsScores).status(200);
+  try {
+    const { offset, limit } = req.query;
+    const scores = await getScores(offset, limit);
+    const catsScores = scores.map(
+      ({ points, nb_matchs: nbMatchs, cat_id: catId }) => {
+        return {
+          winRate: points > 0 ? winRateCalcul(points, nbMatchs) : 0,
+          points,
+          catId,
+        };
+      }
+    );
+    res.send(catsScores).status(200);
+  } catch {
+    res.status(500);
+  }
 };
 
 const winRateCalcul = (points, matchs) => {
